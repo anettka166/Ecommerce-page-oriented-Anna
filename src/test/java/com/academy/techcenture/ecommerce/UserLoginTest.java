be6af95b8c1e240dccd778158aa04ea82818536b
anettka166 <anna_radionova@yahoo.com>
2022-06-01 16:53:52 -0400

    Ecommerce-page-object-oriented project-Anna

---
+++ src/test/java/com/academy/techcenture/ecommerce/UserLoginTest.java
@@ -0,0 +1,53 @@
+package com.academy.techcenture.ecommerce;
+
+
+
+import com.academy.techcenture.ecommerce.config.Driver;
+import com.academy.techcenture.ecommerce.pages.HomePage;
+import com.academy.techcenture.ecommerce.pages.LoginPage;
+import com.academy.techcenture.ecommerce.pages.UserAccountPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class UserLoginTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = Driver.getDriver("chrome");
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @Test(priority = 0)
+    public void userLoginPositive() {
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        UserAccountPage userAccountPage = new UserAccountPage(driver);
+        homePage.clickSingInLink();
+        loginPage.login();
+        userAccountPage.verifyAccountOptions();
+        userAccountPage.navigateHome();
+        homePage.signOut();
+
+    }
+
+    @Test(priority = 1)
+    public void userLoginNegative() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+
+        homePage.clickSingInLink();
+        loginPage.verifyLoginErrors();
+
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        if (driver != null){
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/com/academy/techcenture/ecommerce/UserRegistrationTest.java
@@ -0,0 +1,33 @@
+package com.academy.techcenture.ecommerce;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class UserRegistrationTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        //browser configuration
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        this.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+
+        driver.get("http://automationpractice.com/index.php");
+
+    }
+
+
+    @Test
+    public void test() {
+
+    }
+}
+
+
